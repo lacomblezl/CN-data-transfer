38ebc407000073e09efdd7e14bf67737cdbd759a
lacomblezl <loic.lacomblez@student.uclouvain.be>
2014-10-11 21:28:38 +0200

    Structure et gitignore

---
+++ src/receiver.c
@@ -0,0 +1,28 @@
+/*
+ *  receiver.c
+ *
+ *  INGI2141 - Computer Networks
+ *
+ *  AUTHORS :   Lacomblez Loic (2756-11-00)
+ *              Laurent Quentin (...)
+ *
+ *  DATE : october 2014
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, const char** argv) {
+
+    //TODO getOpt !
+
+
+    //TODO tout le reste...
+
+    printf("Hello, World !\n\n");
+
+    return EXIT_SUCCESS;
+
+}
