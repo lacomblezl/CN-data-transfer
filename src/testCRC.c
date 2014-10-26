1aabfaf9c050fba640fdb3aad7273e7d196b8fcb
lacomblezl <loic.lacomblez@student.uclouvain.be>
2014-10-26 17:41:16 +0100

    Implementation basique CRC

--- src/testCRC.c
+++ src/testCRC.c
@@ -2,9 +2,20 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <string.h>
 #include <zlib.h>
 
 int main(int argc, const char *argv[]) {
+
+    if(argc != 2) {
+        printf("missing argument !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uLong crc = crc32(0L, Z_NULL, 0);
+
+    crc = crc32(crc, (Bytef *) argv[1], strlen(argv[1]));
+    printf("CRC: %lu\n", crc);
 
     return 0;
 }
